0a932c39f4abab61baba9f32099ef64bd283dc79
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-04-10 21:44:11 +0000

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Meals from './components/Meals'
+import Calculator from './components/Calculator'
+
+const App = () => {
+  return (
+    <div>
+      
+      {/* <Meals/> */}
+      <Calculator/>
+    </div>
+  )
+}
+
+export default App
