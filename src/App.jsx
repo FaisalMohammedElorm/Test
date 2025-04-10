2106fcd831a7b79f3157b384dbf1487280750821
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-04-10 22:22:12 +0000

    toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Meals from './components/Meals'
 import Calculator from './components/Calculator'
+import ToggleBackground from './components/ToggleBackground'
 
 const App = () => {
   return (
     <div>
       
-      {/* <Meals/> */}
-      <Calculator/>
+      <ToggleBackground />
+      
     </div>
   )
 }
